3b37d66c000af67f836d5162b1886e1d7b8686a3
ashamsu95 <ashamsuu@gmaill.com>
2023-10-04 17:20:11 +0545

    time change

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
 // Set the date and time for the countdown (change this to your desired date)
-const countdownDate = new Date("2023-12-31 23:59:59").getTime();
+var countDownDate = new Date("Nov 14, 2023 15:37:25").getTime();
 
-// Update the countdown every second
-const timerInterval = setInterval(updateCountdown, 1000);
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
 
-function updateCountdown() {
-  const now = new Date().getTime();
-  const distance = countdownDate - now;
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
 
-  // Calculate days, hours, minutes, and seconds
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Update the HTML elements with the calculated values
+  // Display the result in the element with id="demo"
   document.getElementById("days").innerHTML = days < 10 ? `0${days}` : days;
   document.getElementById("hours").innerHTML = hours < 10 ? `0${hours}` : hours;
-  document.getElementById("minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-  document.getElementById("seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+  document.getElementById("minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.getElementById("seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
 
-  // If the countdown is over, display a message
+  // If the count down is finished, write some text
   if (distance < 0) {
-    clearInterval(timerInterval);
-    document.getElementById("timer").innerHTML = "we are live! Look us up on ur App/Play Store";
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
   }
-}
+}, 1000);
 
-// Initial call to update the countdown immediately
-updateCountdown();
+// const countdownDate = new Date("2023-12-31 23:59:59").getTime();
+
+// // Update the countdown every second
+// const timerInterval = setInterval(updateCountdown, 1000);
+
+// function updateCountdown() {
+//   const now = new Date().getTime();
+//   const distance = countdownDate - now;
+
+//   // Calculate days, hours, minutes, and seconds
+//   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   // Update the HTML elements with the calculated values
+//   document.getElementById("days").innerHTML = days < 10 ? `0${days}` : days;
+//   document.getElementById("hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+//   document.getElementById("minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+//   document.getElementById("seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+//   // If the countdown is over, display a message
+//   if (distance < 0) {
+//     clearInterval(timerInterval);
+//     document.getElementById("timer").innerHTML = "we are live! Look us up on ur App/Play Store";
+//   }
+// }
+
+// // Initial call to update the countdown immediately
+// updateCountdown();
